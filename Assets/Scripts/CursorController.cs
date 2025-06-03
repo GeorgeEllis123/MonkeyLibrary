0dd30ad737eae9cd0d7d2d954ec62a10e5ab3268
Weiss <yuwuif4@gmail.com>
2025-06-03 10:01:33 -0400

    Mouse Update + Rearrangement

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -5,14 +5,34 @@ public class CursorController : MonoBehaviour
     private Vector3 mousePosition;
     public float moveSpeed = 1f;
 
+    public Sprite idleSprite;
+    public Sprite clickSprite;
+
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         Cursor.visible = false;
+
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("ur a silly goose");
+        }
     }
 
     void Update()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+        if (Input.GetMouseButton(0))
+        {
+            spriteRenderer.sprite = clickSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = idleSprite;
+        }
     }
 }
